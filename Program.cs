ac398ebf8e72a2328ae0db5174ce9b7374c9040b
Nick Rudakov <porol2122@gmail.com>
2022-06-29 00:09:34 +0300

    geasДобавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuniorN5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Ann";
+            string secondName = "Deny";
+            Console.WriteLine(firstName + "\t" + secondName);
+            string buffer = firstName;
+            firstName = secondName;
+            secondName = buffer;
+            Console.WriteLine(firstName + "\t" + secondName);
+            Console.ReadLine();
+        }
+    }
+}
